308b593be2d3642b0675fbf1fc62fac5fe1043f4
gerard <12766134+scripting-drafts@users.noreply.github.com>
2025-06-23 21:29:20 +0200

    aws t2.micro

---
+++ AWS/deployment.sh
@@ -0,0 +1,4 @@
+
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y wireless-tools net-tools
+sudo echo -e "alias cls=/usr/bin/clear \nalias python=python3 \nalias pip=pip3" >> ~/.bashrc
